61324b6c3cb1e12ce4a1e428be7215eaaef0babd
Gabriel Flynn <gm.flynn99@yahoo.com>
2020-12-13 16:21:42 -0600

    Login page (#15)
    * initial branch commit
    
    * made some of the initial navbar, still needs some work
    
    * added some color variables and used them for the navBar
    
    * added top bar with some info
    
    * Created a login page -> going to need refactoring for 3rd party integration
    
    * Changed the login page to include login with google, linkedin, microsft, etc. buttons
    
    * Removed extra space
    
    * Remove unnecessary comments
    
    Co-authored-by: Rigre R. Garciandia <rigrergl@gmail.com>

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -13,27 +13,115 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <script src="https://kit.fontawesome.com/45266f9559.js" crossOrigin="anonymous"></script>
     </Head>
     <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
+
+  <nav className="level top-bar m-0">
+  <div className="level-item has-text-centered">
+      {
+      //TODO GET ICONS FOR MAIL AND LOCATION HERE 
+      }
+      <p className="m-1">utdshpe@gmail.com</p>
+      <p className="m-1">800 W Campbell Rd, Richardson, TX 75080</p>
+  </div>
+  </nav>
+
+  <nav className="navbar is-primary" role="navigation" aria-label="main navigation">
+  <div className="navbar-brand">
+    <a className="navbar-item" href="https://bulma.io">
+      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
+    </a>
+
+    {
+    //TODO ADD HAMBURGER HERE 
+    }
+  </div>
+
+  <div id="navbar" className="navbar-menu">
+    <div className="navbar-start">
+      <a className="navbar-item" href="/">
+        Home
+      </a>
+
+      <a className="navbar-item" href="/about">
+        About
+      </a>
+
+      <div className="navbar-item has-dropdown is-hoverable">
+        <a className="navbar-link">
+          Events
+        </a>
+
+        <div className="navbar-dropdown">
+          <a className="navbar-item">
+            Calendar
+          </a>
+          <hr className="navbar-divider"/>
+          <a className="navbar-item">
+            General Meetings
+          </a>
+          <a className="navbar-item">
+            Conferences
+          </a>
+        </div>
+      </div>
+
+      <a className="navbar-item">
+        Membership
+      </a>
+
+      <div className="navbar-item has-dropdown is-hoverable">
+        <a className="navbar-link">
+          Get Involved
+        </a>
+
+        <div className="navbar-dropdown">
+          <a className="navbar-item">
+            Mentor Program
+          </a>
+          <a className="navbar-item">
+            SHPE Jr.
+          </a>
+          <a className="navbar-item">
+            Technology Team
+          </a>
+        </div>
+      </div>
+
+      <a className="navbar-item" href="/users">
+        Users List
+      </a>
+
+      <a className="navbar-item" href="/api/users">
+        Users API
+      </a>
+    </div>
+
+    <div className="navbar-end">
+      <div className="navbar-item">
+        <div className="buttons">
+          <a className="button is-primary">
+            <strong>Sign up</strong>
+          </a>
+          <a className="button is-light" href="/login">
+            Log in
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
     </header>
     {children}
     <footer>
+      <div className="content has-text-centered">
       <hr />
-      <span>I'm here to stay (Footer)</span>
+      <span>
+        I'm here to stay (Footer) 
+        Maybe add some of the contact info here?
+      </span>
+      </div>
     </footer>
   </div>
 )
---
+++ pages/login.tsx
@@ -0,0 +1,26 @@
+import { GoogleLoginButton, LinkedInLoginButton, MicrosoftLoginButton } from 'react-social-login-buttons'
+import Layout from '../components/Layout'
+
+const LoginPage = () => (
+  <Layout title="SHPE UTD">
+        <div className="container">
+            <section className="section mt-4">
+                <div className="columns is-centered is-vcentered">
+                    <div className="column is-half">
+                        <figure className="center-image is-flex is-4 image is-256x256">
+                            <img className="is-rounded" src="/imgs/SHPE_Icon.png" />
+                        </figure>
+                        <p className="title is-1 is-spaced has-text-centered mt-4">Login</p>
+                        <div className="field">
+                            <GoogleLoginButton onClick={() => alert('This will log you in through Google eventually')}/>
+                            <LinkedInLoginButton className="mt-4" onClick={() => alert('This will log you in through LinkedIn eventually')} />
+                            <MicrosoftLoginButton className="mt-4" onClick={() => alert("Maybe we can login with our school outlooks?")} />
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+  </Layout>
+)
+
+export default LoginPage
