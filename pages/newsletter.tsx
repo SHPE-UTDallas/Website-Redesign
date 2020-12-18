85b60bef80fbb8a58747af61e40744a8d59d40ee
Eddie Villarreal <ejv190000@utdallas.edu>
2020-12-18 13:26:07 -0600

    Fixed page styling

--- pages/newsletter.tsx
+++ pages/newsletter.tsx
@@ -9,20 +9,25 @@ const Newsletter = () => (
                     <div className="column is-half">
                         <p className="title is-1 is-spaced has-text-centered mt-4">Join our Newsletter</p>
                         <div className="field">
-                            <p>First name</p>
-                            <input type = "text" id="fname" name="fname"></input>
+                            <label className="label">First name</label>
+                            <div className="control">
+                                <input className="input" type="text" id="fname" name="fname"></input>
+                            </div>
                         </div>
                         <div className="field">
-                            <p>Last name</p>
-                            <input type = "text" id="lname" name="lname"></input>
+                            <label className="label">Last name</label>
+                            <div className="control">
+                                <input className="input" type="text" id="lname" name="lname"></input>
+                            </div>
                         </div>
                         <div className="field">
-                            <p>UTD email</p>
-                            <input type = "text" id="email" name="email"></input>
+                            <label className="label">UTD email</label>
+                            <div className="control">
+                                <input className="input" type="text" placeholder="abc123456@utdallas.edu" id="email" name="email"></input>
+                            </div>
                         </div>
-                        <div className="field">
-                            
-                            <input type = "submit" value="Submit" ></input>
+                        <div className="control">
+                            <button className="button is-primary">Submit</button>
                         </div>
                     </div>
                 </div>
