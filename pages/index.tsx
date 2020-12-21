86b5f9dd1a6a590f4afc05ca440408990d1a63e8
Rigre R. Garciandia <rigrergl@gmail.com>
2020-12-21 02:02:21 -0600

    added gradient background and level element

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,18 @@ import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout title="SHPE UTD">
-  <section className="section">
-    <div className="container">
+  <section className="section level gradient-background">
+    <div className="container level-item has-text-centered">
+      <h1 className="title">
+        Hello World
+      </h1>
+      
+      <p className="subtitle">
+        My first website with <strong>Bulma</strong>!
+      </p>
+    </div>
+
+    <div className="container level-item">
       <h1 className="title">
         Hello World
       </h1>
