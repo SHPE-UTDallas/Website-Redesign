d5fe53e3cf970c763572f8f39ccb59b28002cccf
Rigre R. Garciandia <rigrergl@gmail.com>
2020-12-21 21:43:18 -0600

    fixed some alignment issues on smaller resolutions

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 const IndexPage = () => (
   <Layout title="SHPE UTD">
   <section className="section tile level gradient-background">
-    <div className="container has-text-centered" style={{paddingRight:"20em"}}>
+    <div className="intro container has-text-centered">
     <div className="welcome-text" style={{marginLeft: "2em"}}>
       <h1 className="welcome-text" style={{color: "rgba(0, 0, 0, 0.32)"}}>
         Welcome to
@@ -15,14 +15,16 @@ const IndexPage = () => (
       <p className="subtitle" style={{marginTop: ".75em", marginBottom: "2em"}}>
       Value proposition Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
       </p>
-    </div>
 
-      <button className="button is-primary" style={{margin: "1em"}}>Subscribe to our Newsletter</button>
-      <button className="button is-primary" style={{margin: "1em"}}>Become a sponsor</button>
+      <div className="intro-buttons" style={{marginTop: "2em"}}>
+      <button className="button is-primary center">Subscribe to our Newsletter</button>
+      <button className="button is-primary center" >Become a sponsor</button>
+      </div>
+    </div>
     </div>
 
     <div className="container" style={{paddingRight:"10em"}}>
-      <div className=" tile is-ancestor">
+      <div className=" tile is-ancestor" style={{marginLeft:"2em"}}>
         <div className="homepage-tile tile"></div>
       </div>
     </div>
