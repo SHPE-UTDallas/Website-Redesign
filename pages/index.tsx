0bdcf2554e624d596235896ffcbd13d8a1a83f68
Rigre R. Garciandia <rigrergl@gmail.com>
2020-12-23 17:42:05 -0600

    made the intro paragraph text a bit more gray

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ const IndexPage = () => (
         SHPE UTD
       </h1>
 
-      <p className="subtitle is-4" style={{marginTop: ".75em", marginBottom: "2em"}}>
+      <p className="subtitle is-4" style={{marginTop: ".75em", marginBottom: "2em", color: "rgba(0, 0, 0, 0.61)"}}>
       Value proposition Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
       </p>
 
