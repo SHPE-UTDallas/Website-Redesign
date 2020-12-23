b84b56d4238c0f5920d38c9f9f44f0638c21d7c2
Rigre R. Garciandia <rigrergl@gmail.com>
2020-12-23 17:14:22 -0600

    added the three benefits text below temoc

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Layout from '../components/Layout'
 const IndexPage = () => (
   <Layout title="SHPE UTD">
   <section className="section gradient-background">
+  
     <div className="columns" style={{marginLeft: "5%", marginRight: "5%"}}>
     <div className="intro column has-text-centered">
     <div className="welcome-text">
@@ -18,25 +19,40 @@ const IndexPage = () => (
       Value proposition Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
       </p>
 
-      <div className="intro-buttons" style={{marginTop: "2em"}}>
-      <button className="button is-primary center">Subscribe to our Newsletter</button>
-      <button className="button is-primary center" >Become a sponsor</button>
+      <div className="columns" style={{marginTop: "100px"}}>
+        <div className="column is-half">
+        <button className="button is-primary center">Subscribe to our Newsletter</button>
+        </div>
+        <div className="column is-half">
+        <button className="button is-primary center" >Become a sponsor</button>
+        </div>
       </div>
     </div>
     </div>
 
-    <div className="homepage-tile column"></div>
+    <div className="column">
+      <div className="homepage-tile" style={{marginLeft: "auto", marginRight: "0"}}></div>
     </div>
+    </div>
+
   </section>
 
-  <section className="section level">
-      <div className="" style={{position: "relative", width:"100%", margin:"4%"}}> 
+  <section className="section">
+  <div className="temoc-level">
+      <div style={{position: "relative", width:"100%", paddingLeft:"2%", paddingRight: "2%", height: 10}}> 
         <hr style={{height: .5, borderWidth: 10, display:"block", border: "1px solid rgba(0, 0, 0, 0.17)"}}></hr>
       
         <div className="container" style={{position: "relative", width: "10%", minWidth:"200px", margin:"0 auto", top: "-200px"}}>
           <img  src="/imgs/Temoc_BW.svg" alt="Temoc icon" ></img>
       </div>
       </div>
+  </div>
+
+  <div className="columns is-mobile" style={{paddingLeft: "7%", paddingRight: "7%"}}>
+      <div className="column"><h1 className="benefits-text" style={{textAlign: 'left'}}>Community</h1></div>
+      <div className="column"><h1 className="benefits-text" style={{textAlign: 'center'}}>Networking</h1></div>
+      <div className="column"><h1 className="benefits-text" style={{textAlign: 'right'}}>Career Development</h1></div>
+  </div>
 
   </section>
   </Layout>
