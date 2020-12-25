97409e415e0ade711e2820efcb886c04abc6022a
Rigre R. Garciandia <rigrergl@gmail.com>
2020-12-25 15:20:58 -0600

    centered the calendar in the homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -69,7 +69,9 @@ const IndexPage = () => (
 
   <section className="section" style={{minHeight: "2vw"}}>
     <div className="container">
+      <p style={{textAlign: "center"}}>
     <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FChicago&amp;src=bWFubmVkbG93YUBnbWFpbC5jb20&amp;src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;src=Y2xhc3Nyb29tMTAxNTUyMjg0NjI0OTgyNjUwMDc0QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23039BE5&amp;color=%2333B679&amp;color=%234a148c&amp;color=%230B8043" style={{borderWidth:0}} width="800" height="600" frameBorder="0" scrolling="no"></iframe>
+    </p>
     </div>
   </section>
   </Layout>
