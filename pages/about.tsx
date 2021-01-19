274fd0f83d4d4059971a01a80af0dab465751d89
amoghyatnatti <amoghyatnatti@gmail.com>
2021-01-19 17:24:28 -0600

    Updated Officer Page to print contact information from the json

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,6 +2,9 @@ import Layout from '../components/Layout'
 import OfficerIcon from '../components/OfficerIcon'
 import OfficerJson from '../data/Officer.json'
 
+var presEmail = "";
+var presName = "";
+
 const AboutPage = () => (
   <Layout title="SHPE UTD">
     <div className="has-text-link-dark has-text-centered is-size-1 has-text-weight-bold">
@@ -13,10 +16,15 @@ const AboutPage = () => (
       marginTop: "5px",
       marginRight: "auto"
     }}/>
+    
     <div className="is-flex columns" style={{flexWrap: "wrap", justifyContent: "space-between"}}>
       {
         OfficerJson.map(obj => {
           const {name, position, image, email} = obj
+          if (position == "President") {
+            presEmail = email;
+            presName = name;
+          }
           return (
             <div key={position} className="column is-flex is-one-half-mobile is-one-third-tablet is-one-third-desktop">
               <OfficerIcon name={name} position={position} imgPath={image} email={email}/>
@@ -33,11 +41,11 @@ const AboutPage = () => (
           marginBottom: "10px"}}></hr>
         </span>
         <span className="is-size-5">
-            Join the Freshmen RoundTable to make an impact alongside an officer mentor.
+          Join the Freshmen RoundTable to make an impact alongside an officer mentor.
         </span>
         <div>
-          <a className="button is-rounded is-link" style = {{marginTop: "50px"}} href={`mailto:jvn170000@utdallas.edu`}>
-            Contact
+          <a className="button is-rounded is-link" style = {{marginTop: "50px"}} href={`mailto:${presEmail}`}>
+            Contact {presName}
           </a>
         </div>
       </div>
