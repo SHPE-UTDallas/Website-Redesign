3ab455752047ae3d7ff69c64a73e76afb3053fd2
Rigre R. Garciandia <rigrergl@gmail.com>
2021-01-23 14:26:25 -0600

    created the tech team page (initial commit for this branch)

---
+++ pages/tech-team.tsx
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout'
+
+const TeachTeamPage = () => (
+  <Layout title="Tech team page">
+        <h1>TODO Tech team page</h1>
+  </Layout>
+)
+
+export default TeachTeamPage
