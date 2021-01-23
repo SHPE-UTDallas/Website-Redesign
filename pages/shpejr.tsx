483bab67d1655a589e30fd1a2698aa25875d94c2
techmin <46662857+techmin@users.noreply.github.com>
2021-01-23 15:26:13 -0600

    outlline made inspried from the old shpejr page

--- pages/shpejr.tsx
+++ pages/shpejr.tsx
@@ -1,9 +1,36 @@
 import Layout from "../components/Layout";
 import Link from 'next/link'
 const SHPEJR = () => (
-<Layout title="About | Next.js + TypeScript Example">
-    <h1>SHPE jr</h1>
-    <p>This is the jr page</p>
+<Layout title="SHPEjr | Next.js + TypeScript Example">
+    <h1 
+        className="title is-3"
+     style={{marginLeft: "2%",marginBottom:"2%",marginTop:'2%'}}
+    >
+     SHPEjr 
+    </h1>
+
+    <p className="subtitle is-6"
+       style ={{marginLeft:"2%", marginTop:"1%", }}    
+    >
+      For prospectisve Engineer
+      
+    </p>
+
+  <div
+    style={{marginLeft:".5%"}}
+  >
+   <header 
+      className = "is-size-4"
+      style={{color:"blue"}}
+    >
+       Want to promote STEM to local high school students?    
+     
+   </header>
+
+    <body>
+      Join the SHPE Jr. Committee!
+    </body>
+    </div>
     <p>
       <Link href="/">
         <a>Go home</a>
