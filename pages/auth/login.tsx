01406566730af59b3fac961e0f7d4af1c4e35757
Gabriel Flynn <gm.flynn99@yahoo.com>
2021-01-23 22:21:30 -0600

    Forgot to run prettier

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -21,39 +21,39 @@ interface Providers {
 }
 
 const LoginPage = ({ providers }: Providers) => {
-  const {callbackUrl} = useRouter().query
+  const { callbackUrl } = useRouter().query;
   const callback = Array.isArray(callbackUrl) ? callbackUrl[0] : callbackUrl;
   return (
     <Layout title="SHPE UTD">
-        <section className="section mt-4">
-          <div className="columns is-centered is-vcentered">
-            <div className="column is-half">
-              <figure className="center-image is-flex is-4 image is-256x256">
-                <img className="is-rounded" src="/imgs/SHPE_Icon.png" />
-              </figure>
-              <p className="title is-1 is-spaced has-text-centered mt-4">
-                Login
-              </p>
-              <div className="field">
-                <GoogleLoginButton
-                  onClick={() => signIn(providers.google.id, { callbackUrl: callback })}
-                />
-                <LinkedInLoginButton
-                  className="mt-4"
-                  onClick={() =>
-                    signIn(providers.linkedin.id, { callbackUrl: callback})
-                  }
-                />
-                <MicrosoftLoginButton
-                  className="mt-4"
-                  onClick={() =>
-                    alert("Maybe we can login with our school outlooks?")
-                  }
-                />
-              </div>
+      <section className="section mt-4">
+        <div className="columns is-centered is-vcentered">
+          <div className="column is-half">
+            <figure className="center-image is-flex is-4 image is-256x256">
+              <img className="is-rounded" src="/imgs/SHPE_Icon.png" />
+            </figure>
+            <p className="title is-1 is-spaced has-text-centered mt-4">Login</p>
+            <div className="field">
+              <GoogleLoginButton
+                onClick={() =>
+                  signIn(providers.google.id, { callbackUrl: callback })
+                }
+              />
+              <LinkedInLoginButton
+                className="mt-4"
+                onClick={() =>
+                  signIn(providers.linkedin.id, { callbackUrl: callback })
+                }
+              />
+              <MicrosoftLoginButton
+                className="mt-4"
+                onClick={() =>
+                  alert("Maybe we can login with our school outlooks?")
+                }
+              />
             </div>
           </div>
-        </section>
+        </div>
+      </section>
     </Layout>
   );
 };
--- pages/newsletter.tsx
+++ pages/newsletter.tsx
@@ -1,63 +1,63 @@
-import { useState } from 'react';
-import Layout from '../components/Layout'
-
+import { useState } from "react";
+import Layout from "../components/Layout";
 
 const Newsletter = () => {
-        const [fname, updateName] = useState("");
-        const [lname, updateLName] = useState("");
-        const [email, updateEmail] = useState("");
-        return (
-        <Layout title="SHPE UTD">
-                <section className="section mt-4">
-                    <div className="columns is-centered is-vcentered">
-                        <div className="column is-half">
-                            <p className="title is-1 is-spaced has-text-centered mt-4">Join our Newsletter</p>
-                            <div className="field">
-                                <label className="label">First name</label>
-                                <div className="control">
-                                    <input 
-                                    className="input" 
-                                    type="text" 
-                                    id="fname" 
-                                    name="fname" 
-                                    onChange={(e) => updateName(e.target.value)}> 
-                                    </input>
-                                </div>
-                            </div>
-                            <div className="field">
-                                <label className="label">Last name</label>
-                                <div className="control">
-                                <input 
-                                    className="input" 
-                                    type="text" 
-                                    id="fname" 
-                                    name="fname" 
-                                    onChange={(e) => updateLName(e.target.value)}> 
-                                    </input>
-                                </div>
-                            </div>
-                            <div className="field">
-                                <label className="label">UTD email</label>
-                                <div className="control">
-                                    <input 
-                                    className="input" 
-                                    type="text" 
-                                    placeholder="abc123456@utdallas.edu" 
-                                    id="email" 
-                                    name="email"
-                                    onChange={(e) => updateEmail(e.target.value)}>
-                                    
-                                    </input>
-                                </div>
-                            </div>
-                            <div className="control">
-                                <button className="button is-primary" >Submit</button>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-        </Layout>
-    )
+  const [fname, updateName] = useState("");
+  const [lname, updateLName] = useState("");
+  const [email, updateEmail] = useState("");
+  return (
+    <Layout title="SHPE UTD">
+      <section className="section mt-4">
+        <div className="columns is-centered is-vcentered">
+          <div className="column is-half">
+            <p className="title is-1 is-spaced has-text-centered mt-4">
+              Join our Newsletter
+            </p>
+            <div className="field">
+              <label className="label">First name</label>
+              <div className="control">
+                <input
+                  className="input"
+                  type="text"
+                  id="fname"
+                  name="fname"
+                  onChange={(e) => updateName(e.target.value)}
+                ></input>
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Last name</label>
+              <div className="control">
+                <input
+                  className="input"
+                  type="text"
+                  id="fname"
+                  name="fname"
+                  onChange={(e) => updateLName(e.target.value)}
+                ></input>
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">UTD email</label>
+              <div className="control">
+                <input
+                  className="input"
+                  type="text"
+                  placeholder="abc123456@utdallas.edu"
+                  id="email"
+                  name="email"
+                  onChange={(e) => updateEmail(e.target.value)}
+                ></input>
+              </div>
+            </div>
+            <div className="control">
+              <button className="button is-primary">Submit</button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  );
 };
 
-export default Newsletter
+export default Newsletter;
