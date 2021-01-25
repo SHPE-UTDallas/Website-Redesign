6eca92577d87eee72856451b6fa283d1e4974404
Christian <49210329+cejimenez57@users.noreply.github.com>
2021-01-24 21:14:54 -0600

    Updated Table to be responsive

--- pages/membership.tsx
+++ pages/membership.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from "next/link";
+import Layout from "../components/Layout";
 
 /*
   Heading: How to Join
@@ -9,86 +9,168 @@ import Layout from '../components/Layout'
   Bulma: https://bulma.io/
 */
 
+/*
+  Files on your local computer
+  git status -> What files have changed in that folder since the last "commit"
+
+  commit -> snapshot of changes
+  Changes not staged for commit : Files that you've previously commited or taken a snapshot of
+  untracked files: files that you've never commited or taken a snapshot of
+  add a file to our current "commit" -> be in green if it's added
+  whenever you run "git commit -m "Message here" " -> it'll take a snapshot/add the files that are in green
+  origin
+*/
+
 const MembershipPage = () => (
   <Layout title="SHPE UTD">
-    <h1 className="is-size-3 has-text-centered">How to Join</h1>
-
-    <div className="columns is-centered">
-      <div className="column is-full-mobile is-11-tablet">
-      <h2 className="title is-size-5">Membership Dues</h2>
-      <table className="table is-fullwidth">
-        <thead>
-          <tr>
-            <th style={{width: "33%"}}>Types</th>
-            <th style={{width: "33%"}}>Price</th>
-            <th style={{width: "33%"}}>Payment Options</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Local (UTD Only)</td>
-            <td>$6 Per Semester</td>
-            <td>
-              <ul className="add-ul-bold">
-                <li> <strong>Cash</strong></li>
-                <p>Contact Reyna Garcia (Treasurer)</p>
-                <p>E-Mail:
-                  <a href="mailto:Reyna.Garcia@utdallas.edu?Subject=I%20Would%20Like%20to%20Pay%20my%20Membership%20Dues" target="_blank"> Reyna.Garcia@utdallas.edu</a>
-                </p>
-                <li> <strong>Online</strong> </li>
-              </ul>
-              <p>Please click <a href="https://docs.google.com/forms/d/e/1FAIpQLSdRZXaej0rd4KfcRiI7Sc3qsp_DaPVqPplxjnjAsxEhVbm41g/viewform" target="_blank"> here </a>to fill out our membership form first</p>
-              <p>Venmo: pay at @SHPE-UTD</p>
-            </td>
-          </tr>
-        </tbody>
-        <tbody>
-          <tr>
-            <td>National</td>
-            <td>$10 Annually</td>
-            <td>
-              <ul className="add-ul-bold">
-                <li>
-                  <strong>Online</strong>
-                  <ol className="pl-4">
-                    <li>Go to  <a href="https://www.shpe.org/join-shpe">shpe.org/join-shpe</a></li>
-                    <li>Click "Join Now"</li>
-                    <li>Either log in or follow the registration process</li>
-                    <li>Click "My Membership"</li>
-                  </ol>
-                </li>
-              </ul>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+    <div>
+      <div className="columns is-centered membership-container">
+        <div className="column is-full-mobile is-11-tablet">
+          <div className="join">
+            <br />
+            <h1 className="is-size-3" style={{ fontWeight: "bold" }}>
+              How to Joi<span>n</span>
+            </h1>
+            <br />
+            <ol className="pl-4">
+              <li>
+                Fill out our membership form by clicking{" "}
+                <span className="has-hover">here</span>
+              </li>
+              <li>Pay the local dues below</li>
+              <li>Join SHPE (UTD) on Presence</li>
+              <li>
+                Register with SHPE (national) on{" "}
+                <span className="has-hover">SHPEConnect</span>
+              </li>
+              <li>Pay the national dues below</li>
+            </ol>
+          </div>
+          <br />
+          <br />
+        </div>
       </div>
-    </div>
-    <div className="card">
-      {/* <div className="card-image">
-    <figure className="image is-4by3">
-      <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
-    </figure>
-  </div> */}
-      <div className="card-content is-flex is-flex-direction-row">
-        <div className="media">
-          {/* <div className="media-left">
-            <figure className="image is-48x48">
-              <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
-            </figure>
-          </div> */}
-          <div className="media-content">
-            <p className="title is-4"></p>
-            <p className="subtitle is-6">@johnsmith</p>
+      <h2 className="title is-size-5 membership">Membership Dues</h2>
+      <div className="columns is-flex is-flex-direction-row">
+        <div className="column is-full-tablet is-full-desktop is-half-widescreen">
+          <div className="main-container">
+            <div className="box box1 is-flex-wrap-wrap">
+              <div className="h2-utd">
+                <h2>Local (UTD)</h2>
+              </div>
+              <table className="column">
+                <tr className="columns is-flex is-flex-direction-row">
+                  <th className="has-text-centered column is-half">Price</th>
+                  <th className="has-text-centered column is-half">
+                    Payment Options
+                  </th>
+                </tr>
+                <tr className="columns is-flex">
+                  <td className="border-right column is-half">
+                    <ul className="add-ul-bold">
+                      <li>
+                        <strong>Payment:</strong>
+                      </li>
+                    </ul>
+                    <ol>
+                      <li className="none">$6 Per Semester</li>
+                    </ol>
+                  </td>
+                  <td className="column is-half">
+                    <ul className="add-ul-bold">
+                      <li>
+                        <strong>Cash</strong>
+                      </li>
+                      <ol className="pl-4">
+                        <li>Contact Reyna Garcia (Treasurer)</li>
+                        <li>
+                          E-Mail:
+                          <a
+                            href="mailto:Reyna.Garcia@utdallas.edu?Subject=I%20Would%20Like%20to%20Pay%20my%20Membership%20Dues"
+                            target="_blank"
+                          >
+                            <span className="has-hover">
+                              Reyna.Garcia@utdallas.edu
+                            </span>
+                          </a>
+                        </li>
+                      </ol>
+                    </ul>
+                  </td>
+                </tr>
+                <tr className="columns is-flex">
+                  <td className="border-right column is-half"></td>
+                  <td className="column is-half">
+                    {" "}
+                    <ul className="add-ul-bold">
+                      <li>
+                        <strong>Online</strong>
+                      </li>
+                      <ol className="pl-4">
+                        <li>
+                          Please click <span className="has-hover">here</span>{" "}
+                          to fill out our membership form first
+                        </li>
+                        <li>
+                          Venmo: pay at{" "}
+                          <span className="has-hover">@SHPE-UTD</span>
+                        </li>
+                      </ol>
+                    </ul>
+                  </td>
+                </tr>
+              </table>
+            </div>
           </div>
         </div>
-
-        <div className="content">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-      <a href="#">#css</a> <a href="#">#responsive</a>
-          <br />
-          <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+        <div className="column is-full-tablet is-full-desktop  is-half-widescreen">
+          <div className="main-container ">
+            <div className="box box2 ">
+              <div className="h2-local">
+                <h2>National</h2>
+              </div>
+              <table>
+                <tr>
+                  <th className="has-text-centered">Price</th>
+                  <th className="has-text-centered">Payment Options</th>
+                </tr>
+                <tr>
+                  <td className="border-right">
+                    <ul className="add-ul-bold">
+                      <li>
+                        <strong>Payment:</strong>
+                      </li>
+                    </ul>
+                    <ol>
+                      <li className="none">$10 Annually</li>
+                    </ol>
+                  </td>
+                  <td>
+                    <ul className="add-ul-bold">
+                      <li>
+                        <strong>Online</strong>
+                        <ol className="pl-4">
+                          <li>
+                            Go to{" "}
+                            <a href="https://www.shpe.org/join-shpe">
+                              <span className="has-hover">
+                                shpe.org/join-shpe
+                              </span>
+                            </a>
+                          </li>
+                          <li>Click "Join Now"</li>
+                          <li>
+                            Either log in or follow the registration process
+                          </li>
+                          <li>Click "My Membership"</li>
+                        </ol>
+                      </li>
+                    </ul>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -100,6 +182,81 @@ const MembershipPage = () => (
       </Link>
     </p> */}
   </Layout>
-)
+);
 
-export default MembershipPage
+/*<table className="table is-fullwidth" style={{ borderRadius: "2%" }}>
+          <thead>
+            <tr>
+              <th style={{ width: "33%" }}>Types</th>
+              <th style={{ width: "33%" }}>Price</th>
+              <th style={{ width: "33%" }}>Payment Options</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Local (UTD Only)</td>
+              <td>$6 Per Semester</td>
+              <td>
+                <ul className="add-ul-bold">
+                  <li>
+                    {" "}
+                    <strong>Cash</strong>
+                  </li>
+                  <p>Contact Reyna Garcia (Treasurer)</p>
+                  <p>
+                    E-Mail:
+                    <a
+                      href="mailto:Reyna.Garcia@utdallas.edu?Subject=I%20Would%20Like%20to%20Pay%20my%20Membership%20Dues"
+                      target="_blank"
+                    >
+                      {" "}
+                      Reyna.Garcia@utdallas.edu
+                    </a>
+                  </p>
+                  <li>
+                    {" "}
+                    <strong>Online</strong>{" "}
+                  </li>
+                </ul>
+                <p>
+                  Please click{" "}
+                  <a
+                    href="https://docs.google.com/forms/d/e/1FAIpQLSdRZXaej0rd4KfcRiI7Sc3qsp_DaPVqPplxjnjAsxEhVbm41g/viewform"
+                    target="_blank"
+                  >
+                    {" "}
+                    here{" "}
+                  </a>
+                  to fill out our membership form first
+                </p>
+                <p>Venmo: pay at @SHPE-UTD</p>
+              </td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr>
+              <td>National</td>
+              <td>$10 Annually</td>
+              <td>
+                <ul className="add-ul-bold">
+                  <li>
+                    <strong>Online</strong>
+                    <ol className="pl-4">
+                      <li>
+                        Go to{" "}
+                        <a href="https://www.shpe.org/join-shpe">
+                          shpe.org/join-shpe
+                        </a>
+                      </li>
+                      <li>Click "Join Now"</li>
+                      <li>Either log in or follow the registration process</li>
+                      <li>Click "My Membership"</li>
+                    </ol>
+                  </li>
+                </ul>
+              </td>
+            </tr>
+          </tbody>
+        </table>*/
+
+export default MembershipPage;
