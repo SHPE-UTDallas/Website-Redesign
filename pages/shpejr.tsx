0c6940ad5c10fc0961935785fe6c50ed41cc6c15
techmin <46662857+techmin@users.noreply.github.com>
2021-01-25 22:26:04 -0600

    add the HS form link and fixed margin on the whole page

--- pages/shpejr.tsx
+++ pages/shpejr.tsx
@@ -16,8 +16,16 @@ const SHPEJR = () => (
       
     </p>
 
+    <hr
+            style={{
+              height: 0.5,
+              borderWidth: 10,
+              border: "1px solid rgba(0, 0, 0, 0.17)",
+            }}
+          ></hr>
+
   <div
-    style={{marginLeft:".5%"}}
+    style={{marginLeft:".5%", marginBottom:"2em"}}
   >
    <header 
       className = "is-size-4"
@@ -31,7 +39,24 @@ const SHPEJR = () => (
       Join the SHPE Jr. Committee!
     </body>
     </div>
-    <p>
+  
+    <ul className="actions"
+      style={{marginLeft:".5%", marginTop:"15px"}}
+    >
+       <li>
+         <a href="https://docs.google.com/forms/d/1piiE0I5lyUuZWxgP0DRDyi1vlRLDlkhnuwAAPQdqQec" className="button is-primary is-outlined" target="_blank">
+            Fill out this form
+         </a>
+            </li>
+      </ul>
+    
+    <p
+    style={{marginTop:"10px"}}
+    >
+      You can also contact the SHPE Jr. Officers
+    </p>
+
+      <p>
       <Link href="/">
         <a>Go home</a>
       </Link>
