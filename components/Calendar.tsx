8f1427627e40d98e0706b2c99e374f83a8fe673c
Mahir Morar <MahirMorar17@users.noreply.github.com>
2021-01-28 18:18:42 -0600

    Fixed Google API Calendar issue

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 import FullCalendar from '@fullcalendar/react'
 import interactionPlugin from '@fullcalendar/interaction'
 import timeGridPlugin from '@fullcalendar/timegrid'
+import googleCalendarPlugin from '@fullcalendar/google-calendar'
 
 export default () => (
   <FullCalendar
-    plugins={[interactionPlugin, timeGridPlugin]}
+    plugins={[interactionPlugin, timeGridPlugin, googleCalendarPlugin]}
     initialView='timeGridWeek'
+    googleCalendarApiKey= '1094491606059-faucm7iq4tfroidsq68jve9s6kblkjhs.apps.googleusercontent.com'
     nowIndicator={true}
-    editable={true}
-    initialEvents={[
-      { title: 'nice event', start: new Date() }
-    ]}
+    editable={false}
+
   />
 )
--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -2,11 +2,12 @@ import Link from 'next/link'
 import Layout from '../components/Layout'
 import FullCalendar from '../components/Calendar'
 import React from 'react'
+import googleCalendarPlugin from '../components/Calendar'
 
 
 
 const AboutPage = () => (
-  <Layout title="About | Next.js + TypeScript Example">
+  <Layout title="SHPE Calendar">
     <FullCalendar/>
   </Layout>
 )
