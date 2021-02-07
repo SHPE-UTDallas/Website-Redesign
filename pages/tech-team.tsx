ed969d4ffdbc023f1018cea7a2c5d41112b72239
Rigre Garciandia <rigrergl@outlook.com>
2021-02-06 19:48:05 -0600

    ran prettier

--- pages/tech-team.tsx
+++ pages/tech-team.tsx
@@ -1,19 +1,23 @@
-import Layout from '../components/Layout'
+import Layout from "../components/Layout";
 import OfficerJson from "../data/Officer.json";
 import OfficerIcon from "../components/OfficerIcon";
 import TechProject from "../components/TechProject";
 import ProjectsJson from "../data/TechTeamProjects.json";
 
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 type Props = {
   techOfficerName: string;
   techOfficerEmail: string;
   imgPath: string;
-}
+};
 
-const TeachTeamPage = ({ techOfficerEmail, techOfficerName, imgPath }: Props) => (
+const TeachTeamPage = ({
+  techOfficerEmail,
+  techOfficerName,
+  imgPath,
+}: Props) => (
   <Layout title="Tech team page">
     <section className="section">
       <OfficerIcon
@@ -26,18 +30,31 @@ const TeachTeamPage = ({ techOfficerEmail, techOfficerName, imgPath }: Props) =>
 
     <section className="section">
       <div className="container" style={{ textAlign: "center" }}>
-        The technology team is in charge of implementing and maintaining software solutions for the club (including the club website).
-        Members get the opportunity to <strong>work in a team setting with industry standard technologies</strong>. Very little previous experience is required
-        in order join, as the team is meant to be a learning experience. Outside of our weekly meetings <strong>(Saturdays 2:00pm-3:00pm CST)</strong>, we require
-        very little time commitment from our members, everyone can work as much or as little as they want.
+        The technology team is in charge of implementing and maintaining
+        software solutions for the club (including the club website). Members
+        get the opportunity to{" "}
+        <strong>
+          work in a team setting with industry standard technologies
+        </strong>
+        . Very little previous experience is required in order join, as the team
+        is meant to be a learning experience. Outside of our weekly meetings{" "}
+        <strong>(Saturdays 2:00pm-3:00pm CST)</strong>, we require very little
+        time commitment from our members, everyone can work as much or as little
+        as they want.
       </div>
 
-
-      <div className="container buttons is-centered" style={{ marginTop: "40px", textAlign: "center" }}>
+      <div
+        className="container buttons is-centered"
+        style={{ marginTop: "40px", textAlign: "center" }}
+      >
         <a className="button" href="https://www.google.com/" target="_blank">
           <span>Join the team</span>
         </a>
-        <a className="button" href="https://github.com/SHPE-UTDallas" target="_blank">
+        <a
+          className="button"
+          href="https://github.com/SHPE-UTDallas"
+          target="_blank"
+        >
           <span className="icon">
             <FontAwesomeIcon icon={faGithub} />
           </span>
@@ -48,7 +65,15 @@ const TeachTeamPage = ({ techOfficerEmail, techOfficerName, imgPath }: Props) =>
 
     <section className="section projects">
       <div className="container">
-        <h1 className="projects-title title" style={{ textAlign: "center", fontSize: "40px", textDecoration: "underline", marginBottom: "40px" }}>
+        <h1
+          className="projects-title title"
+          style={{
+            textAlign: "center",
+            fontSize: "40px",
+            textDecoration: "underline",
+            marginBottom: "40px",
+          }}
+        >
           Projects
         </h1>
 
@@ -74,7 +99,7 @@ const TeachTeamPage = ({ techOfficerEmail, techOfficerName, imgPath }: Props) =>
       </div>
     </section>
   </Layout>
-)
+);
 
 export async function getStaticProps() {
   let techOfficerName = "";
@@ -92,9 +117,9 @@ export async function getStaticProps() {
     props: {
       techOfficerName,
       techOfficerEmail,
-      imgPath
+      imgPath,
     },
   };
 }
 
-export default TeachTeamPage
+export default TeachTeamPage;
