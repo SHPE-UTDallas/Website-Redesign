7f94ec83604f44b3cd4885047672979d7e71e2d0
Gabriel Flynn <gm.flynn99@yahoo.com>
2021-03-15 20:20:53 -0500

    Added sponsor.tsx

---
+++ pages/sponsor.tsx
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+
+const SponsorPage = () => (
+  <Layout title="SHPE UTD">
+    <p>Sponsor 1</p>
+    <p>Sponsor 2</p>
+  </Layout>
+);
+
+
+export default AboutPage;
