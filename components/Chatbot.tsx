9eb84a2203b4ca2ec9845b5462b3c0caeec8b459
Rigre Garciandia <rigrergl@outlook.com>
2021-03-19 22:40:05 -0500

    added the corporate liaison officer icon at the end of the non-student paths

--- components/Chatbot.tsx
+++ components/Chatbot.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "styled-components";
 import ChatBot from "react-simple-chatbot";
 import Link from "next/link";
 import OfficerJson from "../data/Officer.json";
+import OfficerIcon from "../components/OfficerIcon";
 import { triggerAsyncId } from "node:async_hooks";
 
 const theme = {
@@ -34,9 +35,26 @@ export default function App() {
   };
 
   const officerIcon = (title: string) => {
+    let officerName = "";
+    let officerEmail = "";
+    let imgPath = "";
+
+    OfficerJson.map((obj) => {
+      const { name, position, email, image } = obj;
+      if (position === title) {
+        officerName = name;
+        officerEmail = email;
+        imgPath = image;
+      }
+    });
     return (
       <div>
-        <p>{title}</p>
+        <OfficerIcon
+          name={officerName}
+          position={title}
+          imgPath={imgPath}
+          email={officerEmail}
+        />
       </div>
     );
   };
@@ -46,7 +64,7 @@ export default function App() {
       <ChatBot
         floating={true}
         opened={true}
-        botAvatar="/imgs/Dora.png"
+        botAvatar="/imgs/SHPE_Logo.png"
         headerTitle="SHPE UTD Chatbot"
         floatingIcon="/imgs/SHPE_Logo.png"
         bubbleStyle={{ maxWidth: "75%" }}
@@ -61,7 +79,11 @@ export default function App() {
             id: "company_or_student",
             options: [
               { value: "Student", label: "Student", trigger: "student" },
-              { value: "Professional", label: "Professional", trigger: "professional" },
+              {
+                value: "Professional",
+                label: "Professional",
+                trigger: "professional",
+              },
               { value: "Recruiter", label: "Recruiter", trigger: "recruiter" },
               { value: "Company", label: "Company", trigger: "company" },
             ],
@@ -171,17 +193,20 @@ export default function App() {
           },
           {
             id: "professional",
-            message: "Thank you for visiting our website! We welcome professionals to come and talk to our members as well as attend our events. Contact our corporate liaison for more information:",
-            trigger: "corporate_liaison"
+            message:
+              "Thank you for visiting our website! We welcome professionals to come and talk to our members as well as attend our events. Contact our corporate liaison for more information:",
+            trigger: "corporate_liaison",
           },
           {
             id: "recruiter",
-            message: "Thank you for visiting our website! Sponsor companies have access to our ResumeBook website, a collection of all of our member's resumes. Checkout this demo of our ResumeBook website. For full access to our real database of members, please contact our corporate liaison.",
+            message:
+              "Thank you for visiting our website! Sponsor companies have access to our ResumeBook website, a collection of all of our member's resumes. Checkout this demo of our ResumeBook website. For full access to our real database of members, please contact our corporate liaison.",
             trigger: "resume_book_link",
           },
           {
             id: "company",
-            message: "Thank you for visiting our website! We offer a variety of services for our sponsors, including access to our ResumeBook website. Please contact our corporate liaison for more information about the benefits of being a sponsor as well as on how to become one: ",
+            message:
+              "Thank you for visiting our website! We offer a variety of services for our sponsors, including access to our ResumeBook website. Please contact our corporate liaison for more information about the benefits of being a sponsor as well as on how to become one: ",
             trigger: "resume_book_link",
           },
           {
@@ -190,13 +215,18 @@ export default function App() {
           },
           {
             id: "resume_book_demo",
-            message: "Checkout this demo of our ResumeBook website. For full access to our real database of members, please contact our corporate liaison: ",
-            trigger: "resume_book_link"
+            message:
+              "Checkout this demo of our ResumeBook website. For full access to our real database of members, please contact our corporate liaison: ",
+            trigger: "resume_book_link",
           },
           {
             id: "resume_book_link",
-            component: button("ResumeBook Demo", "https://shpe-utdallas.github.io/ResumeBook/", true),
-            trigger: "corporate_liaison"
+            component: button(
+              "ResumeBook Demo",
+              "https://shpe-utdallas.github.io/ResumeBook/",
+              true
+            ),
+            trigger: "corporate_liaison",
           },
         ]}
       />
