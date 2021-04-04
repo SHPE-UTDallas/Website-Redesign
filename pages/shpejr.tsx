87f0eacc7fc617fade0880ed9b3a426486143dc8
techmin <46662857+techmin@users.noreply.github.com>
2021-04-03 22:39:26 -0500

    page layout center and unionize button k

--- pages/shpejr.tsx
+++ pages/shpejr.tsx
@@ -1,5 +1,4 @@
 import Layout from "../components/Layout";
-import Link from 'next/link'
 import OfficerJson from "../data/Officer.json";
 
 
@@ -12,106 +11,69 @@ type Props = {
 
 const SHPEJR = ({jrName,jrEmail}:Props) => ( 
 <Layout title="SHPEjr">
-    <h1 
-        className="title is-3"
-     style={{marginLeft: "2%",marginBottom:"2%",marginTop:'2%'}}
+    <div className="has-text-link-dark has-text-centered is-size-1 has-text-weight-bold"
+      style={{marginBottom:".7em"}}
     >
-     SHPEjr 
-    </h1>
-
-    <p className="subtitle is-6"
-       style ={{marginLeft:"2%", marginTop:"1%", }}    
-    >
-      For prospectisve Engineer
-      
-    </p>
-
-    <hr
-            style={{
-              height: 0.5,
-              borderWidth: 10,
-              border: "1px solid rgba(0, 0, 0, 0.17)",
-            }}
-          ></hr>
-  <div
-    className='columns'
-  >
-    <div
-      className='column'
-    >
-  <div
-    style={{marginLeft:".5%", marginBottom:"2em"}}
-  >
+      SHPEjr
+    </div>
+<div
+  className="container"
+>     
+     <p
+      style ={{textAlign:"center", marginBottom:".5em", marginLeft:"20%",marginRight:"20%",fontSize:"18px"}}
+     >
+     At SHPE UTD, SHPE Jr is our high school outreach program, where we work with local SHPE Jr chapters 
+   	 to organize engineering projects, provide academic/professional guidance, and help students foster 
+	   a successful future in STEM.The Noche de Ciencias is our premier event, where a whole evening is 
+	   dedicated to both the high schoolers and their parents. For the students,we have sessions with 
+	   professional engineers and engineering workshops.At the same time, events are held for the parents, 
+     answering their questions and discussing the benefits and easability of their children going into higher education and STEM.
+     </p>
+     </div>
    <header 
       className = "is-size-4"
-      style={{color:"blue"}}
+      style={{color:"blue",marginLeft:"42%",marginBottom:"1em"}}
     >
-       Want to promote STEM to local high school students?    
-     
+        Promote STEM in your HIgh School!
    </header>
 
-    <body>
-      Join the SHPE Jr. Committee!
-    </body>
-    </div>
-  
-    <ul className="actions"
-      style={{marginLeft:".5%", marginTop:"15px"}}
+   <div
+    className="level"
+    style={{marginLeft:"42% ", marginRight:"30%"}}
+   > 
+    <ul className=" level-left"
+      //style={{marginRight:"10%"}}
     >
-       <li>
-         <a href="https://docs.google.com/forms/d/1piiE0I5lyUuZWxgP0DRDyi1vlRLDlkhnuwAAPQdqQec" className="button is-primary is-outlined" target="_blank">
-            Fill out this form
+         <a href="https://docs.google.com/forms/d/1piiE0I5lyUuZWxgP0DRDyi1vlRLDlkhnuwAAPQdqQec"
+          //className="button is-primary is-rounded"  
+          target="_blank"
+        //  style={{color:"white"}}
+          >
+            <button
+                className="button is-primary is-rounded"  
+              >      
+                    Join Committee Member!
+            </button>
+
          </a>
-            </li>
       </ul>
    
-    <p
-    style={{marginTop:"10px"}}
-    >
-      You can also contact the SHPE Jr. Officers
-    </p>
-   
-    <div
-       style={{marginRight:"80%", marginLeft:"1em"}}
-    >
+      <ul
+        className="actions  level-right"
+      //  style={{marginRight:"20%"}}
+      > 
       <a
-          className="button is-rounded is-link"
           href={`mailto:${jrEmail}`}
-          >
-             Contact {jrName}
-
-      </a>    
-    </div>
-  </div>
-      
-   <div
-    className='column'
-    style={{marginRight:"10%",fontSize:"120%"}}
-   >
-     <p
-     // style ={{marginLeft:'80%'}}
-     >
-     At SHPE UTD, SHPE Jr is our high school outreach program, 
-     where we work with local SHPE Jr chapters to organize engineering projects, 
-     provide academic/professional guidance, and help students foster a successful future in STEM. 
-     The Noche de Ciencias is our premier event, where a whole evening is dedicated to both 
-     the high schoolers and their parents. For the students, 
-     we have sessions with professional engineers and engineering workshops.
-      At the same time, events are held for the parents, 
-     answering their questions and discussing the benefits and 
-     easability of their children going into higher education and STEM.
-     </p>
-
-   </div>
-  </div>
-    
-      <p>
-    
-      <Link href="/">
-        <a>Go home</a>
-      </Link>
-    </p>
-
+        >
+        ,<button
+         className="button is-primary is-rounded"
+         >
+                    Contact {jrName}
+          </button>
+        </a>
+    </ul>
+     </div> 
+     
   </Layout>
 )
 export async function getStaticProps() {
