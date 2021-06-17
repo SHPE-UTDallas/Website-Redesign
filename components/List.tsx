dfb1db518582f729864eca37c399fda160417386
Rigre Garciandia <rigrergl@outlook.com>
2021-06-17 03:47:08 -0500

    temporary fix to List module

--- components/List.tsx
+++ components/List.tsx
@@ -1,32 +1,21 @@
-// import * as React from 'react'
-// import ListItem from './ListItem'
+import * as React from 'react'
+import ListItem from './ListItem'
 // import { User } from '../interfaces'
+import { User } from 'next-auth'
 
-// type Props = {
-//   items: User[]
-// }
-
-// const List = ({ items }: Props) => (
-//   <ul>
-//     {items.map((item) => (
-//       <li key={item.id}>
-//         <ListItem data={item} />
-//       </li>
-//     ))}
-//   </ul>
-// )
-
-// export default List
-
-import React from 'react'
-
-const List = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+type Props = {
+  items: User[]
 }
+
+const List = ({ items }: Props) => (
+  <ul>
+    {items.map((item) => (
+      <li key={item.id}>
+        <ListItem data={item} />
+      </li>
+    ))}
+  </ul>
+)
 
 export default List
 
--- components/ListDetail.tsx
+++ components/ListDetail.tsx
@@ -1,28 +1,18 @@
-// import * as React from 'react'
+import * as React from 'react'
 
 // import { User } from '../interfaces'
+import { User } from 'next-auth'
 
-// type ListDetailProps = {
-//   item: User
-// }
-
-// const ListDetail = ({ item: user }: ListDetailProps) => (
-//   <div>
-//     <h1>Detail for {user.name}</h1>
-//     <p>ID: {user.id}</p>
-//   </div>
-// )
-
-// export default ListDetail
-
-import React from 'react'
-
-const ListDetail = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+type ListDetailProps = {
+  item: User
 }
 
+const ListDetail = ({ item: user }: ListDetailProps) => (
+  <div>
+    <h1>Detail for {user.name}</h1>
+    <p>ID: {user.id}</p>
+  </div>
+)
+
 export default ListDetail
+
