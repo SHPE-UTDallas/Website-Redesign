bf72aec9bfdfc81db9dc57aa3af23276b806bbcb
techmin <46662857+techmin@users.noreply.github.com>
2021-07-23 02:01:28 -0500

    layout completed

--- pages/podcast.tsx
+++ pages/podcast.tsx
@@ -4,25 +4,31 @@ import OfficerJson from "../data/Officer.json";
 const Podcast = () =>(
     <Layout title="SHPE Podcast">
          <div className="has-text-link-dark has-text-centered title is-1 has-text-weight-bold"
-      style={{ marginBottom: ".7em" }}
+      style={{ marginBottom: ".75em" }}
     >
       Podcast
     </div>
-<div className="tile ">
-  <div className="tile is-10 box">
-    <div className="has-test-centere">
-      empty
+    <div
+      className="container"
+      >
+    <div
+      className=" is-flex column is-flex-wrap-wrap" 
+    > 
+      <div
+        className=" is-flex is-half "
+      >
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/xov60UwCQXs" title="YouTube video player" 
+        allow="accelerometer  ; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
+          </iframe>
+     </div>
     </div>
-  </div> 
-</div>
-
-
-<section className="section">
-<div className="container" style={{textAlign:"center"}}>
-  Listen your fellow SHPE member and officer talk about future events.
-  Discussion of latest tech and tools used in the industry.
-</div>
-</section>
+  </div>
+  
+<div className="container" style={{textAlign:"center", marginTop:"2em"}}>
+  Get closer to  la familia with the podcast! Listen to fellow member discussing
+  and chatting of latest industrial and on-campus news.  
+  Get insight on job oppotunity and interview process. 
+  </div>  
     </Layout>
 );
 
