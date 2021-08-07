3b439c52b359887fda5120e178074f22fe0b0c40
techmin <46662857+techmin@users.noreply.github.com>
2021-08-06 23:36:24 -0500

    video and paragraph responsive

--- pages/podcast.tsx
+++ pages/podcast.tsx
@@ -8,26 +8,28 @@ const Podcast = () =>(
       Podcast
     </div>
     <div
-      className="container podcastVideo"
+      className="container podcastVideo  "
       >
     <div
-      className="is-felx column is-flex-wrap-wrap" 
+      className="  is-felx column is-flex-wrap-wrap" 
       style={{marginLeft:"10%"}}
+      
     > 
       <div
         className=" is-flex  is-half"
       >
-      <iframe width="780" height="428" src="https://www.youtube-nocookie.com/embed/xov60UwCQXs" title="YouTube video player"
+      <iframe width="650" height="315" src="https://www.youtube-nocookie.com/embed/xov60UwCQXs" title="YouTube video player"
        frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen className="website"></iframe>
      </div>
     </div>
-  </div>
+    </div>
   
-<div className="container is-flex" style={{textAlign:"center", marginTop:"2em", marginRight:"5%", fontSize:"150%" }}>
+<div className="container is-flex is-half" style={{textAlign:"center", marginTop:"2em",marginLeft:"20%",marginRight:"15%",fontSize:"125%" }}>
   Get closer to  la familia with the podcast! Listen to fellow member discussing
   and chatting on latest industrial and on-campus news.  
-  Along with that g et insight on job oppotunity and interview process. 
+  Along with that get insight on job oppotunity and interview process. 
   </div>  
+
     </Layout>
 );
 
